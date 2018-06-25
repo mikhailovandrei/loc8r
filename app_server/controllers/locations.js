d8d56e056cd24e2131b2c0341d740f03c48cbc45
Mikhailovandrei <mih6.4@yandex.ru>
2018-06-25 19:49:05 +0300

    4commit

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,11 @@
 /* Получить (GET) домашнюю страницу */
 module.exports.homelist = function(req, res){
-res.render('locations-list', { title: 'Home' });
+res.render('locations-list', { title: 'Loc8r - find a place to work with wifi',
+pageHeader: {
+title: 'Loc8r',
+strapline: 'Find places to work with wifi near you!'
+}
+});
 };
 /* Получить (GET) страницу с информацией о местоположениях */
 module.exports.locationInfo = function(req, res){
